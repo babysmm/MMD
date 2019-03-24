301cdaa4b219ac75050cd07a17d7b6ab8cbeef2a
mm-china <30348056+babysmm@users.noreply.github.com>
2019-03-24 16:06:51 +0800

    eureka

--- mmdshop-provider-8001/src/main/java/com/mmd/mmdshop/Shop8001_App.java
+++ mmdshop-provider-8001/src/main/java/com/mmd/mmdshop/Shop8001_App.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.annotation.MapperScan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableEurekaClient
 @MapperScan("com.mmd.mmdshop.mapper")
 public class Shop8001_App
 {
